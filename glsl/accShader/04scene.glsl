c902865ea5557088762e3b953d810ed4a3c94165
stevejtrettel <stevejtrettel@gmail.com>
2020-12-14 17:50:18 -0800

    put back balls

--- glsl/accShader/04scene.glsl
+++ glsl/accShader/04scene.glsl
@@ -1,9 +1,31 @@
+//-------------------------------------------------
+//The LIGHTS
+//-------------------------------------------------
 
 
 
 
 
 
+
+//-------------------------------------------------
+//The OBJECTS
+//-------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------
+//The SCENE
+//-------------------------------------------------
 
 
 
@@ -19,87 +41,87 @@ float sceneSDF(Vector tv, inout localData dat){
     float roughness;
     float intensity;
     
-//    //sphere 0
-//    vec3 center=vec3(0,-0.35,-2.);
-//    float dist= sphereSDF(tv, center,0.5);
-//    
-//    
-//    if(dist<eps){
-//        
-//        dat.isSky=false;
-//        dat.normal=sphereNormal(tv,center);
-//        
-//        color=0.5*vec3(0.9,0.9,0.5);
-//        specularity=0.2;
-//        roughness=0.4;
-//        
-//        setDielectric(dat.mat, color, specularity, roughness);
-//        
-//        return dist;
-//    }
-//    
-//    
-//        //sphere 1
-//    vec3 center1=vec3(-0.6,-0.63,-1.6);
-//    float dist1= sphereSDF(tv, center1,0.25);
-//    
-//    
-//    if(dist1<eps){
-//        dat.isSky=false;
-//        dat.normal=sphereNormal(tv,center1);
-//        
-//        color=vec3(0.3,0.2,0.6);
-//        specularity=0.5;
-//        roughness=0.05;
-//        
-//        setMetal(dat.mat, color, specularity, roughness);
-//        
-//        return dist1;
-//    }
-//
-//    dist1=min(dist,dist1);
-//    
-//    
-//            //sphere 1
-//    vec3 center11=vec3(-0.1,-0.7,-1.3);
-//    float dist11= sphereSDF(tv, center11,0.15);
-//    
-//    
-//    if(dist11<eps){
-//        dat.isSky=false;
-//        dat.normal=sphereNormal(tv,center11);
-//        
-//        color=0.75*vec3(0.7,0.1,0.2);
-//        specularity=0.6;
-//        roughness=0.05;
-//        
-//        setMetal(dat.mat, color, specularity, roughness);
-//
-//        return dist11;
-//    }
-//
-//    dist1=min(dist1,dist11);
-//    
+    //sphere 0
+    vec3 center=vec3(0,-0.35,-2.);
+    float dist= sphereSDF(tv, center,0.5);
     
     
-    vec2 rad=vec2(1,.02);
-    float height=.2;
-    vec3 center=vec3(0,-0.6,-1.);
-    
-    float dist1=ringSDF(tv,rad,height,center);
-    
-     if(dist1<eps){
-        dat.isSky=false;
-        dat.normal=ringNormal(tv,rad,height,center);
+    if(dist<eps){
         
-        color =vec3(.6,.6,0.2);
-        specularity=0.75;
-        roughness=0.0;
+        dat.isSky=false;
+        dat.normal=sphereNormal(tv,center);
+        
+        color=0.5*vec3(0.9,0.9,0.5);
+        specularity=0.2;
+        roughness=0.4;
+        
+        setDielectric(dat.mat, color, specularity, roughness);
+        
+        return dist;
+    }
+    
+    
+        //sphere 1
+    vec3 center1=vec3(-0.6,-0.63,-1.6);
+    float dist1= sphereSDF(tv, center1,0.25);
+    
+    
+    if(dist1<eps){
+        dat.isSky=false;
+        dat.normal=sphereNormal(tv,center1);
+        
+        color=vec3(0.3,0.2,0.6);
+        specularity=0.5;
+        roughness=0.05;
         
         setMetal(dat.mat, color, specularity, roughness);
         
         return dist1;
     }
+
+    dist1=min(dist,dist1);
+    
+    
+            //sphere 1
+    vec3 center11=vec3(-0.1,-0.7,-1.3);
+    float dist11= sphereSDF(tv, center11,0.15);
+    
+    
+    if(dist11<eps){
+        dat.isSky=false;
+        dat.normal=sphereNormal(tv,center11);
+        
+        color=0.75*vec3(0.7,0.1,0.2);
+        specularity=0.6;
+        roughness=0.05;
+        
+        setMetal(dat.mat, color, specularity, roughness);
+
+        return dist11;
+    }
+
+    dist1=min(dist1,dist11);
+    
+    
+//    
+//    vec2 rad=vec2(1,.02);
+//    float height=.2;
+//    vec3 center=vec3(0,-0.6,-1.);
+//    
+//    float dist1=ringSDF(tv,rad,height,center);
+//    
+//     if(dist1<eps){
+//        dat.isSky=false;
+//        dat.normal=ringNormal(tv,rad,height,center);
+//        
+//        color =vec3(.6,.6,0.2);
+//        specularity=0.75;
+//        roughness=0.0;
+//        
+//        setMetal(dat.mat, color, specularity, roughness);
+//        
+//        return dist1;
+//    }
     
     
     
